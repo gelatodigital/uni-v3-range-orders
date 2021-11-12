4e8b0024d1befbbee1d2f10de061d99ef8a92e48
Shivva <matthieumariejoseph@gmail.com>
2021-11-12 14:45:53 +0100

    sm deployment

--- src/addresses.ts
+++ src/addresses.ts
@@ -32,15 +32,15 @@ export const getAddresses = (network: string): Addresses => {
       };
     case "arbitrum":
       return {
-        PokeMe: "",
+        PokeMe: "0xB3f5503f93d5Ef84b06993a1975B9D21B962892F",
         NonfungiblePositionManager:
           "0xC36442b4a4522E871399CD717aBDD847Ab11FE88",
         UniswapV3Factory: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         SwapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        Gelato: "0x3CACa7b48D0573D793d3b0279b5F0029180E83b6",
-        WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        DAI: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
-        USDT: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+        Gelato: "0x4775aF8FEf4809fE10bf05867d2b038a4b5B2146",
+        WETH: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+        DAI: "",
+        USDT: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
         EjectLP: "",
         RangeOrders: "",
         RangeOrdersResolver: "",
